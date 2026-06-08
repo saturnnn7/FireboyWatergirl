a1c65759dfa6604c2679f953ccc8b0bdecc8e97c
saturnnn7 <soprikin.kirill@gmail.com>
2026-06-09 01:38:49 +0200

    feat(client): remove duplicate PlayerType, reference FBW.Shared enums

--- src/FBW.Client/Entities/PlayerEntity.cs
+++ src/FBW.Client/Entities/PlayerEntity.cs
@@ -1,2 +1,19 @@
+using Nez;
+using FBW.Shared.Enums;
 
 namespace FBW.Client.Entities;
+
+/// <summary>
+/// Represents a player entity (Fireboy or Watergirl).
+/// </summary>
+public class PlayerEntity : Entity
+{
+    public PlayerType Type { get; private set; }
+
+    public PlayerEntity(PlayerType type)
+    {
+        Type = type;
+    }
+
+    // TODO: add components (physics, input, animator, collider)
+}
--- src/FBW.Client/Game1.cs
+++ src/FBW.Client/Game1.cs
@@ -2,7 +2,7 @@
 
 namespace FBW.Client;
 
-public class Game1 : Core
+public class Game1 : Nez.Core
 {
     protected override void Initialize()
     {
